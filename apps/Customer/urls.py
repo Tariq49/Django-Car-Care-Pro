dbc87a9c8b52e6df2be2ae2a87d355f9bf47af37
Ginu <ginujestin@gmail.com>
2024-07-23 22:13:14 +0200

    sign up data stored to database

--- apps/Customer/urls.py
+++ apps/Customer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import customer_home,delete_account
+from .views import customer_home,delete_account,SignUpView
 
 urlpatterns = [
     path('customer/', customer_home, name='customer_home'),
     path('delete-account/', delete_account, name='delete_account'),
+    path('signup/', SignUpView, name='signup'),
 ]
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import home_view,signup_view,login_view,about_us,website,logout_view
+from .views import home_view,about_us,website,logout_view
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
+    path('', home_view, name='home'), 
     path('website/', website, name='website'),
     path('about/', about_us, name='about_us'),
     path('logout/', logout_view, name='logout'),
